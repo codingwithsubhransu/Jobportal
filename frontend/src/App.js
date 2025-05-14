9b18348148fc5dc8a1d48949ace7eb6a92585fde
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2025-05-14 23:34:53 +0530

    'hii'

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import AllRouters from './Routes/AllRouters';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRouters />
+    </div>
+  );
+}
+
+export default App;
